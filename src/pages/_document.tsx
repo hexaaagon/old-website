ad8566d8417ad3d5d0604ed588ab200f3d229032
Hexagonn <128217934+hexaaagon@users.noreply.github.com>
2023-09-24 05:00:41 +0000

    Add analytics plugin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from '@vercel/analytics/react';
 import * as Icon from "react-feather";
 
 export default function Document() {
@@ -8,6 +9,7 @@ export default function Document() {
       <body className="light bg-gray-300 dark:bg-gray-800">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
